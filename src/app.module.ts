144d1ac9f307c46566b75adf855fed171eab8f53
Lukas Dias <diaslukas19@gmail.com>
2022-12-16 19:32:02 -0300

    * first day

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule } from './http.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
